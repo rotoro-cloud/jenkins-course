fec3ea813c5c4e6b8418a023e62e82e4aa602924
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-07-17 21:51:52 +0300

    Update themeSwitcher.js

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,17 +1,8 @@
 window.addEventListener('message', function(event) {
-    console.log(event.data); // Message received from parent
-
-
-    document.body.innerHTML = '<h1>Hello ' + event.data.status + 'Inner HTML!<h1>'
-    document.body.classList.add('bb-dark-theme');
     if (event.data.status == 'dark' ) { 
         document.body.classList.add('bb-dark-theme');
-        console.log("set dark")
-        console.log(document.body.style)
     } else {
         document.body.classList.remove('bb-dark-theme');
-        console.log("set ligth")
-        console.log(document.body.style)
     }
     
 });
